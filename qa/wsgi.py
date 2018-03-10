6f460d41708ff9bd37840432ea5e1f99103c3dec
piterskikhsa <piterskikhsa@mail.ru>
2018-03-10 19:35:52 +0300

    Update wsgi.py

--- qa/wsgi.py
+++ qa/wsgi.py
@@ -8,10 +8,8 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 import os
-from configurations.wsgi import get_wsgi_application
-
+from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "qa.settings")
-os.environ.setdefault('DJANGO_CONFIGURATION', 'Dev')
 
 application = get_wsgi_application()
